887dbf035446cc66a0b00ae677dc722587a9fc6d
thosijulio <thosijulio@gmail.com>
2022-08-02 20:10:03 -0300

    feat - criacao da tela sobre mim

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
 import Header from './components/Header';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   );
