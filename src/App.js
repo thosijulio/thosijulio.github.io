0c2a9937f49f99dc1fabfc11fa02e2108813aee3
thosijulio <thosijulio@gmail.com>
2022-08-04 21:56:55 -0300

    feat - configurando tela de projetos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Projects from './pages/Projects';
 import Header from './components/Header';
 
 function App() {
@@ -8,8 +9,9 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </>
   );
---
+++ src/pages/Projects.js
@@ -0,0 +1,17 @@
+import './Projects.css';
+
+function Projects() {
+  return (
+    <section className="projects-section">
+      <div className="projects-main-text">
+        <span className="main-text">
+          Projetos
+        </span>
+        <div className="project-role">
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Projects;
