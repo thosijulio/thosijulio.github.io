4443f2e1ba71e138ede601ba5d9416e715feb1c1
thosijulio <thosijulio@gmail.com>
2024-06-01 11:15:19 -0300

    fix header e instalacao react icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import showHeaderNavBar from '../helpers/showHeaderNavBar';
-import './Header.css';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import showHeaderNavBar from "../helpers/showHeaderNavBar";
+import { MdMenu } from "react-icons/md";
+import "./Header.css";
 
 function Header() {
   const [showDropDownMenu, setShowDropDownMenu] = useState(true);
@@ -11,27 +12,40 @@ function Header() {
   }, [showDropDownMenu]);
 
   useEffect(() => {
-    const dropDownBtnStyle = getComputedStyle(document.querySelector('#drop-down-btn'));
-    if (dropDownBtnStyle.display === 'block') {
+    const dropDownBtnStyle = getComputedStyle(
+      document.querySelector("#drop-down-btn")
+    );
+    if (dropDownBtnStyle.display === "block") {
       setShowDropDownMenu(false);
     }
   }, []);
 
   const handleNavClick = () => {
-    const dropDownBtnStyle = getComputedStyle(document.querySelector('#drop-down-btn'));
-    if (dropDownBtnStyle.display === 'block') {
+    const dropDownBtnStyle = getComputedStyle(
+      document.querySelector("#drop-down-btn")
+    );
+    if (dropDownBtnStyle.display === "block") {
       setShowDropDownMenu(false);
     }
-  }
+  };
 
   return (
     <header>
-      <p onClick={() => setShowDropDownMenu(!showDropDownMenu)}>Menu</p>
+      <MdMenu id="drop-down-btn"
+        onClick={() => setShowDropDownMenu(!showDropDownMenu)} />
       <nav id="header-nav-bar">
-        <Link onClick={handleNavClick} to="/">Home</Link>
-        <Link onClick={handleNavClick} to="/about">Sobre</Link>
-        <Link onClick={handleNavClick} to="/projects">Projetos</Link>
-        <Link onClick={handleNavClick} to="/contact">Contato</Link>
+        <Link onClick={handleNavClick} to="/">
+          Home
+        </Link>
+        <Link onClick={handleNavClick} to="/about">
+          Sobre
+        </Link>
+        <Link onClick={handleNavClick} to="/projects">
+          Projetos
+        </Link>
+        <Link onClick={handleNavClick} to="/contact">
+          Contato
+        </Link>
       </nav>
     </header>
   );
